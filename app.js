59f08ecccc2df48e9ae2017caef0313504c1a1ad
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2023-12-15 16:02:07 +0700

    initialize express app

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoDb = process.env.MONGODB_URI;
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
