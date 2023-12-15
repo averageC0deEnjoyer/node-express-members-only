4047b509a70e2bcdaf00b9c93d5793743837c15a
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2023-12-15 16:08:26 +0700

    add error handler middleware, follow from MDN

--- app.js
+++ app.js
@@ -23,3 +23,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+//catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+//error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  //render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
